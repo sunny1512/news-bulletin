1dce511e3076c975cba8ab6245974e57c25faf4c
prince13207 <prince0078956@gmail.com>
2018-08-17 11:27:11 +0530

    added sort and filter endpoints

--- news-bulletin-app/routes/newsrouter.js
+++ news-bulletin-app/routes/newsrouter.js
@@ -19,4 +19,35 @@ newsinfo.find({})
 )
 
 
+newsRouter.route('/sort')
+.get((req,res,next)=>{
+
+newsinfo.find({}).sort('publisher')
+.then((news)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','application/json');
+    
+    res.json(news);
+},(err)=>next(err))
+.catch((err)=>next(err));
+}
+)
+
+newsRouter.route('/:publisher_name')
+.get((req,res,next)=>{
+console.log(req.params.publisher_name);
+newsinfo.find({publisher : req.params.publisher_name})
+.then((news)=>{
+    res.statusCode=200;
+    res.setHeader('Content-Type','application/json');
+    
+    res.json(news);
+},(err)=>next(err))
+.catch((err)=>next(err));
+}
+)
+
+
+
+
 module.exports= newsRouter;
